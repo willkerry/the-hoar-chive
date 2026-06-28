e83bf124c8cfcd78cdcb0d440172403a461ade03
Will Kerry <24854452+willkerry@users.noreply.github.com>
2026-06-28 19:54:11 +0100

    feat: add experimental SVG optimizer and content intellisense to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, svgoOptimizer } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -7,6 +7,10 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   build: { assets: "_hoard" },
+  experimental: {
+    contentIntellisense: true,
+    svgOptimizer: svgoOptimizer(),
+  },
   site: "https://thehoar.org",
   image: {
     domains: ["thehoar.org"],
